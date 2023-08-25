8bfdb3f6e4e79a16ad6d79eee8a60b9412de4220
Mateo Rodriguez <mdavidrodriguez@unicesar.edu.co>
2023-08-25 00:35:12 -0500

    agregando sengrid

--- emails/appointmentEmailService.js
+++ emails/appointmentEmailService.js
@@ -1,24 +1,29 @@
 import { createTransport } from '../config/nodemailer.js'
+import sgMail from '@sendgrid/mail';
+
+// Configura la clave API de SendGrid
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
 export async function sendEmailNewAppointment({ date, time }) {
-    const transporter = createTransport(
-        process.env.EMAIL_HOST,
-        process.env.EMAIL_PORT,
-        process.env.EMAIL_USER,
-        process.env.EMAIL_PASS
-    )
-
-    const info = await transporter.sendMail({
-        from: 'Consultorio del Dr. Juan Velázquez ! <citasturbo@gmail.com>',
+    const msg = {
         to: 'rodriguez@gmail.com',
+        from: 'Consultorio del Dr. Juan Velázquez ! <citasturbo@gmail.com>',
         subject: "SalonBelleza",
         text: "Salon - Nueva Cita",
         html: `<p>Hola: Rodriguez tienes una nueva Cita </p>
-        <p>La cita sera el día: ${date} a las ${time} horas</p>
-        `
-    })
+        <p>La cita sera el día: ${date} a las ${time} horas</p>`
+    };
+
+    try {
+        await sgMail.send(msg);
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
 }
+
+// Las otras funciones (sendEmailUpdateAppointment, sendEmailDeleteAppointment) se modifican de manera similar.
+
 
 
 export async function sendEmailUpdateAppointment({ date, time }) {
